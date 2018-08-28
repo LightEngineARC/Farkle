8049ee4089aa8a5882ee7840fd0398d5560e453a
Ashton Chatelain <34180527+LightEngineARC@users.noreply.github.com>
2018-08-27 20:10:44 -0600

    setting up project space On branch master Changes to be committed: new file:   .classpath modified:   .gitignore new file:   .project new file:   .settings/org.eclipse.jdt.core.prefs new file:   src/Game/PlayFarkle.java

---
+++ src/Game/PlayFarkle.java
@@ -0,0 +1,47 @@
+/********************************************************
+ *
+ *  Project :  <Name of project|assignment>
+ *  File    :  <Name of source code file>
+ *  Name    :  Ashton Chatelain
+ *  Date    :  <Date created (project due date)>
+ *
+ *  Description : (Narrative description, not code)
+ *
+ *    1) What is the purpose of the code; what problem does the code solve.
+ *
+ *    2) What data-structures are used.
+ *
+ *    3) What algorithms, techniques, etc. are used in implementing the data structures.
+ *
+ *    4) What methods are implemented (optional).
+ *
+ *  Changes :  <Description|date of modifications>
+ *
+ ********************************************************/
+package Game;
+
+/**
+ * @author iamsp
+ *
+ */
+public class PlayFarkle
+{
+
+	/****************************************************
+	 * Method : Main
+	 *
+	 * Purpose : run a game of Farkle
+	 *
+	 * Parameters : type - description number_of_elements - the number of elements
+	 * in the type
+	 *
+	 * Returns : What does it return
+	 *
+	 ****************************************************/
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
