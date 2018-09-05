bd7c09a6d6174a2147b12913a026d1b869bfde28
Ashton Chatelain <34180527+LightEngineARC@users.noreply.github.com>
2018-09-05 17:16:02 -0600

    modified:   src/Game/scoring.java started scoring to check for multiples and store them in an array

--- src/Game/scoring.java
+++ src/Game/scoring.java
@@ -20,10 +20,50 @@
  ********************************************************/
 package Game;
 
+import java.util.Arrays;
+
 /**
  * @author Ashton
  *
  */
 public class scoring {
+	// accept an array of dice and out put best score
+	int score = 0;
+	int[] dice = new int[6];
 
+	public int scoreDice(int[] diceRecieved) {
+		// TODO score the dice
+		Arrays.sort(diceRecieved);
+		int multiRollArray[] = { 0, 0, 0, 0, 0, 0 };
+
+		for (int i = 0; i < 6; i++) {
+			switch (diceRecieved[i]) {
+			case 1:
+				multiRollArray[0]++;
+				break;
+			case 2:
+				multiRollArray[1]++;
+				break;
+			case 3:
+				multiRollArray[2]++;
+				break;
+			case 4:
+				multiRollArray[3]++;
+				break;
+			case 5:
+				multiRollArray[4]++;
+				break;
+			case 6:
+				multiRollArray[5]++;
+				break;
+			default:
+				System.out.println("not a valid die number");
+				break;
+			}
+
+		}
+
+		return score;
+
+	}
 }
