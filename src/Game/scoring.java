147c4ac67480da722b9ddd485f01058bb883c3b5
Ashton Chatelain <34180527+LightEngineARC@users.noreply.github.com>
2018-09-05 16:47:52 -0600

    new file:   src/Game/computer.java 	new file:   src/Game/scoring.java

---
+++ src/Game/scoring.java
@@ -0,0 +1,29 @@
+/********************************************************
+ *
+ *  Project :  <Name of project|assignment>
+ *  File    :  <Name of source code file>
+ *  Name    :  Ashton Chatelain
+ *  Date    :  <Date created (project due date)>
+ *
+ *  Description : (Narrative description, not code)
+ *
+ *    1) What is the purpose of the code; what problem does the code solve.
+ *
+ *    2) What data-structures are used.
+ *
+ *    3) What algorithms, techniques, etc. are used in implementing the data structures.
+ *
+ *    4) What methods are implemented (optional).
+ *
+ *  Changes :  <Description|date of modifications>
+ *
+ ********************************************************/
+package Game;
+
+/**
+ * @author Ashton
+ *
+ */
+public class scoring {
+
+}
