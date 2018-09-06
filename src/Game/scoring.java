baea13347259310edd55b2c4d100e27a117a9a3f
Ashton Chatelain <34180527+LightEngineARC@users.noreply.github.com>
2018-09-06 12:03:37 -0600

    added TODOs to grab final score options. needs to be tested.
    modified:   src/Game/scoring.java

--- src/Game/scoring.java
+++ src/Game/scoring.java
@@ -34,8 +34,11 @@ public class scoring {
 	public int scoreDice(int[] diceRecieved) {
 		// TODO score the dice
 		Arrays.sort(diceRecieved);
+
+		// stores number of times each number is rolled
 		int multiRollArray[] = { 0, 0, 0, 0, 0, 0 };
 
+		// count of each number to find multiple rolls
 		for (int i = 0; i < 6; i++) {
 			switch (diceRecieved[i]) {
 			case 1:
@@ -57,10 +60,65 @@ public class scoring {
 				multiRollArray[5]++;
 				break;
 			default:
-				System.out.println("not a valid die number");
+				System.out.println("die locked");
 				break;
 			}
+		}
 
+		// check for the high value situations and cascade through lower options
+		for (int j = 0; j < 6; j++) {
+			if (multiRollArray[j] == 6) {
+				score = 3000;
+			} else if (multiRollArray[j] == 5) {
+				score = 2000;
+			} else if (multiRollArray[j] == 4) {
+				score = 1000;
+				// check for a double for an extra 500 points
+				for (int k = 0; k < 6; k++) {
+					if (multiRollArray[k] == 2) {
+						score = score + 500;
+					}
+				}
+			} else if (multiRollArray[j] == 3) {
+				switch (multiRollArray[j]) {
+				case 0:
+					score = 300;
+					break;
+				case 1:
+					score = 200;
+					break;
+				case 2:
+					score = 300;
+					break;
+				case 3:
+					score = 500;
+					break;
+				case 4:
+					score = 500;
+					break;
+				case 5:
+					score = 600;
+					break;
+
+				}
+				// TODO add check for two triplets
+			}
+		}
+		if (diceRecieved[0] == 1 && diceRecieved[1] == 2 && diceRecieved[2] == 3 && diceRecieved[3] == 4
+				&& diceRecieved[4] == 5 && diceRecieved[5] == 6) {
+			score = 1500;
+
+		}
+
+		// check for 3 pair
+		int pairCount = 0;
+		for (int l = 0; l < 6; l++) {
+			if (multiRollArray[l] == 2) {
+				pairCount++;
+			}
+		}
+		if (pairCount == 3) {
+			score = 1500;
 		}
 
 		return score;
